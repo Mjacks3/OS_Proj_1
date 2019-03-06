2942839a5647f9e309a256be321280b958b56cb8
RJ <joyce8@umbc.edu>
2019-03-06 15:33:19 -0500

    Added CreateSite, finished ReadSite

--- proj1.go
+++ proj1.go
@@ -5,27 +5,73 @@ import (
     "net/http"
     "database/sql"
     _ "github.com/go-sql-driver/mysql"
-    //TODO: will need "encoding/json"
+    "encoding/json"
     "github.com/gorilla/mux"
 )
 
 
-type Site struct {
-    Name string `json:"name,omitempty"`
-    Role string `json:"role,omitempty"`
-    URI string `json:"uri,omitempty"`
-    AP *AccessPoint `json:"ap"` // TODO: Allow 0+ access points
+type AccessPoint struct {
+    Label string `json:"label"`
+    URL string `json:"url"`
 }
 
-type AccessPoint struct {
-    Label string `json:"label,omitempty"`
-    URL string `json:"url,omitempty"`
+type Site struct {
+    Name string `json:"name"`
+    Role string `json:"role"`
+    URI string `json:"uri"`
+    APs []AccessPoint `json:"ap"`
 }
 
 
 func CreateSite(w http.ResponseWriter, r *http.Request){
+
     fmt.Println("Called CreateSite!")
-    // TODO
+
+    // Decode request JSON
+    var site Site
+    decoder := json.NewDecoder(r.Body)
+    err := decoder.Decode(&site)
+    if err != nil{
+        fmt.Println(err)
+    }
+
+    // Open MySQL database
+    db, err := sql.Open("mysql", "proj1user:password@/proj1")
+    if err != nil {
+        fmt.Println(err)
+    }
+    defer db.Close()
+
+    // Prepare MySQL statement for creating site
+    query := "INSERT INTO sites (name, role, uri) VALUES (?, ?, ?)"
+    stmt, err := db.Prepare(query)
+    if err != nil {
+        fmt.Println(err)
+    }
+    defer stmt.Close()
+
+    // Execute MySQL statement
+    _, err = stmt.Exec(site.Name, site.Role, site.URI)
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    // Iterate through access points
+    for _, AP := range site.APs{
+
+        // Prepare MySQL statement for creating access point
+	query = "INSERT INTO site_aps (ap, url, name) VALUES (?, ?, ?)"
+	stmt, err = db.Prepare(query)
+        if err != nil {
+            fmt.Println(err)
+        }
+
+        // Execute MySQL statement
+        _, err = stmt.Exec(AP.Label, AP.URL, site.Name)
+        if err != nil {
+            fmt.Println(err)
+        }
+    }
 }
 
 
@@ -43,34 +89,69 @@ func ReadSite(w http.ResponseWriter, r *http.Request){
     }
     defer db.Close()
 
-    // Query MySQL db
-    query := "SELECT name, role, uri, ap FROM sites NATURAL LEFT JOIN " +
-             "site_aps WHERE name=?"
-    rows, err := db.Query(query, request["name"])
+    // Prepare MySQL query for sites
+    query := "SELECT name, role, uri FROM sites WHERE name=?"
+    stmt, err := db.Prepare(query)
+    if err != nil {
+        fmt.Println(err)
+    }
+    defer stmt.Close()
+
+    // Retrieve query results
+    var name string
+    var role string
+    var uri string
+    err = stmt.QueryRow(request["name"]).Scan(&name, &role, &uri)
+    if err != nil{
+        fmt.Println(err)
+    }
+    fmt.Println("result:", name, role, uri)
+
+    // Prepare MySQL query for site's access points
+    query = "SELECT ap, url FROM site_aps WHERE name=?"
+    stmt, err = db.Prepare(query)
+    if err != nil {
+        fmt.Println(err)
+    }
+    defer stmt.Close()
+
+    // Retrieve query results
+    var APs []AccessPoint
+    var label string
+    var url string
+    rows, err := stmt.Query(request["name"])
+    if err != nil {
+        fmt.Println(err)
+    }
+    for rows.Next(){
+        err := rows.Scan(&label, &url)
+        if err != nil {
+            fmt.Println(err)
+        }
+        var AP AccessPoint
+	AP.Label = label
+	AP.URL = url
+	APs = append(APs, AP)
+    }
+
+    // Construct JSON response
+    var response Site
+    response.Name = name
+    response.Role = role
+    response.URI = uri
+    response.APs = APs
+    response_json, err := json.Marshal(response)
     if err != nil {
         fmt.Println(err)
     }
 
-    // TODO: Parse query results into JSON
-    var(
-        name string
-        role string
-        uri string
-        ap string
-    )
+    // Send response to client
+    w.Header().Set("Content-Type", "application/json")
+    fmt.Fprintf(w, string(response_json))
 
-    // Just printing the results for now
-    for rows.Next(){
-        err := rows.Scan(&name, &role, &uri, &ap)
-        // TODO: An error is thrown here if NULL is inserted into ap
-        if err != nil{
-            fmt.Println(err)
-	}
-	fmt.Println("result:", name, role, uri, ap)
-    }
-
-    // TODO: Send response to client
+    // TODO: Shouldn't send a response if no results in db
 }
+
 
 func UpdateSite(w http.ResponseWriter, r *http.Request){
     fmt.Println("Called UpdateSite!")
@@ -106,44 +187,14 @@ func DeleteSite(w http.ResponseWriter, r *http.Request){
 }
 
 
-func CreateAP(w http.ResponseWriter, r *http.Request){
-    fmt.Println("Called CreateAP!")
-    // TODO
-}
-
-
-func ReadAP(w http.ResponseWriter, r *http.Request){
-    fmt.Println("Called ReadAP!")
-    // TODO
-}
-
-
-func UpdateAP(w http.ResponseWriter, r *http.Request){
-    fmt.Println("Called UpdateAP!")
-    // TODO
-}
-
-
-func DeleteAP(w http.ResponseWriter, r *http.Request){
-    fmt.Println("Called DeleteAP!")
-    // TODO
-}
-
-
 func main(){
     router := mux.NewRouter()
 
     // Handlers for site API calls
-    router.HandleFunc("/site/{name}", CreateSite).Methods("POST")
+    router.HandleFunc("/site/", CreateSite).Methods("POST")
     router.HandleFunc("/site/{name}", ReadSite).Methods("GET")
-    //router.HandleFunc("/site/{name}", UpdateSite).Methods("POST") ???
+    router.HandleFunc("/site/{name}", UpdateSite).Methods("POST") // ???
     router.HandleFunc("/site/{name}", DeleteSite).Methods("Delete")
-
-    // Handlers for access point API calls
-    router.HandleFunc("/ap/{label}", CreateSite).Methods("POST")
-    router.HandleFunc("/ap/{label}", ReadSite).Methods("GET")
-    //router.HandleFunc("/ap/{label}", UpdateSite).Methods("POST") ???
-    router.HandleFunc("/ap/{label}", DeleteSite).Methods("Delete")
 
     // Listen on port 8000 for REST API calls
     fmt.Println("Listening on 127.0.0.1:8000")
